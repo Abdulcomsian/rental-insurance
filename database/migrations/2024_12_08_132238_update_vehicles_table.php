df89534d8f837de744d1066a7b1b419bd3ed1c3c
msaeedinfomist <saeed.mudassar@gmail.com>
2024-12-08 18:39:28 +0500

    migration fixing

--- database/migrations/2024_12_08_132238_update_vehicles_table.php
+++ database/migrations/2024_12_08_132238_update_vehicles_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
                 $table->integer('insurance_company_id')->nullable;  
 
                 
-                       });
+                       }); 
         });
     }
 
